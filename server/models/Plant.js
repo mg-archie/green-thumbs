47d1af5754911df8705dc75326f438fa30d698bc
Mike5719 <149823846+Mike5719@users.noreply.github.com>
2024-02-08 10:47:45 -0500

    user, plant, blog, comment models completed

---
+++ server/models/Plant.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+
+const plantSchema = new Schema({
+  name: {
+      type: String,
+    },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  image: {
+    type: String,
+  },
+  lowLight: {
+    type: boolean,
+    required: true,
+  },
+  indoor: {
+    type: boolean,
+    required: true,
+  },
+});
+
+const Plant = model('Plant', plantSchema);
+
+module.exports = Plant;
---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Plant = require('./Plant');
+const Comment = require('./Comment');
+const Blog = require('./Blog');
+
+module.exports = { User, Plant, Comment, Blog };
