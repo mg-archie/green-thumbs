7192abc3d1050a55aec5631d9897e5532faf7f74
arkenwald <149994852+arkenwald@users.noreply.github.com>
2024-02-08 13:27:39 -0500

    created login and signup components client-side, and updated package.json (global)

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -18,3 +18,25 @@ module.exports = {
     ],
   },
 }
+
+
+
+//from template
+/*
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'react/prop-types': 'off',
+  },
+}
+*/
