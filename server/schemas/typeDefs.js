451b858fc5342766636e5656dc75b938512a4bc5
arkenwald <149994852+arkenwald@users.noreply.github.com>
2024-02-08 14:13:41 -0500

    typedefs template and refactor

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,56 @@
+const typeDefs = `
+type User {
+  _id: ID
+  username: String
+  email: String
+  plantCount: Int
+  favouritedPlants: [Plant]
+}
+
+type Plant {
+  plantId: String!
+  description: String
+  name: String!
+  image: String
+  lowLight: Boolean
+  indoor: Boolean
+  link: String
+}
+
+type Comment{
+
+}
+
+type Blog {
+    
+}
+
+type Auth {
+  token: ID!
+  user: User
+}
+
+type Query {
+  me: User
+  users: [User]
+  user(username: String!): User
+}
+
+input PlantInput {
+  plantId: String!
+  authors: [String]
+  description: String
+  title: String!
+  image: String
+  link: String
+}
+
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  favouritedPlants(plantInput: PlantInput): User
+  removePlant(plantId: ID!): User
+}
+`;
+
+module.exports = typeDefs;
