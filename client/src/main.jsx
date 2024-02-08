6e5043ba7496227afbbd2759a8fc1c0a8f85c18f
arkenwald <149994852+arkenwald@users.noreply.github.com>
2024-02-08 12:30:43 -0500

    updated navbar.jsx and searchPlants.jsx with templates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import App from './App.jsx'
-import './index.css'
+import SearchBooks from './pages/SearchBooks'
+import SavedBooks from './pages/SavedBooks'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <SearchBooks />
+      }, {
+        path: '/saved',
+        element: <SavedBooks />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
